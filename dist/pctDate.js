23eb84799cb75022caec2c91b63d4bcbe6cedf78
franleplant <franleplant@gmail.com>
2014-12-12 13:31:39 -0300

    add new dists files and a visual example of the tz selector directive

--- dist/pctDate.js
+++ dist/pctDate.js
@@ -33,6 +33,7 @@
 (function() {
     'use strict';
 
+
     /*
      * @ngdoc module
      * @name pctDate
@@ -104,3 +105,252 @@
 
 
 })();
+
+(function() {
+    'use strict';
+
+    angular.module('pctDate.timezoneSelector.controller', [
+        'pctDate.utils.tzId'
+        ])
+        .controller('_pctTimezoneSelectorDirectiveController', controller);
+
+
+
+    controller.$inject = ['$scope', 'getTzList', 'filterTzByRegion']
+
+
+    function controller($scope, getTzList, filterTzByRegion) {
+
+        this.selectedRegion = 'America';
+
+        this.tzRegionList = getTzList().regionList;
+
+        var tzList = getTzList().tzList;
+
+        this.getTzListForRegion = filterTzByRegion(tzList);
+
+
+        //explain why ngModel in the tpl is used with the scope instead of the ctrl
+
+    }
+
+}) ();
+
+(function() {
+
+    angular.module('pctDate.timezoneSelector.directive', [
+        //Auto generated template module for testing and distribution purporses
+        'pctDate.templates',
+        'pctDate.timezoneSelector.controller'
+        ])
+        .directive('pctTimezoneSelector', directiveDef);
+
+
+    function directiveDef() {
+
+
+
+
+        function link(scope, element, attrs, ngModelCtrl) {
+
+        }
+
+        return {
+            restrict: 'E',
+            require: 'ngModel',
+            controller: '_pctTimezoneSelectorDirectiveController',
+            controllerAs: 'ctrl',
+            templateUrl: 'src/timezoneSelector/timezoneSelector.tpl.html',
+            link: link,
+
+            //TODO: explain why we use two way data binding for ngModel (=) and text binding
+            //for class (@)
+            //https://umur.io/angularjs-directives-using-isolated-scope-with-attributes/
+            scope: {
+                ngModel: '=ngModel',
+                clazz: '@class'
+            }
+
+        }
+    }
+
+
+
+}) ();
+
+(function() {
+
+    angular.module('pctDate.timezoneSelector', [
+            'pctDate.timezoneSelector.directive'
+        ]);
+
+}) ();
+
+(function() {
+    'use strict';
+
+
+    angular.module('pctDate.utils.tzId.filterTzByRegion', [])
+        .factory('filterTzByRegion', factory);
+
+
+    function factory() {
+
+
+        return function filterTzByRegionFactory(tzList) {
+
+            if (!angular.isArray(tzList)) {
+                throw new TypeError('pctDate.filterTzByRegion expects 1 argument type Array');
+            }
+
+
+            return function filterTzByRegion(selectedRegion) {
+                return tzList.filter(function(element) {
+                    return element.region === selectedRegion
+                });
+
+            };
+        }
+
+
+
+    }
+
+
+})();
+
+(function() {
+
+
+    angular.module('pctDate.utils.tzId.getTzList', [
+            'pctMoment',
+            'pctDate.utils.tzId.parseTzIdList',
+            'pctDate.utils.tzId.removeTzIdSpecialCases'
+        ])
+        .factory('getTzList', factory);
+
+
+    factory.$inject = ['moment', 'parseTzIdList', 'removeTzIdSpecialCases'];
+
+    //TODO: check that moment timezone is loaded
+    function factory(moment, parseTzIdList, removeTzIdSpecialCases) {
+        //http://momentjs.com/timezone/docs/#/data-loading/getting-zone-names/
+        var tzListRaw = moment.tz.names();
+
+        var aux = parseTzIdList(removeTzIdSpecialCases(tzListRaw));
+        var tzRegionList = aux[0];
+        var tzList = aux[1]
+
+        return function getTzList() {
+
+            return {
+                regionList: aux[0],
+                tzList: aux[1]
+            }
+        }
+    }
+
+})();
+
+(function() {
+    'use strict';
+
+    angular.module('pctDate.utils.tzId.parseTzId', [])
+        .factory('parseTzId', factory);
+
+    function factory() {
+        var aux;
+
+        return function(tzId) {
+            aux = tzId.match(/(^[^\/]*)\/(.*)/);
+
+            return {
+
+                id: tzId,
+                region: aux[1],
+                subregion: aux[2]
+            };
+        }
+    }
+
+})();
+
+(function() {
+    'use strict';
+
+    angular.module('pctDate.utils.tzId.parseTzIdList', [
+            'pctDate.utils.tzId.parseTzId'
+        ])
+        .factory('parseTzIdList', factory);
+
+
+
+    factory.$inject = ['parseTzId']
+
+    function factory(parseTzId) {
+
+        return function parseTzIdList(tzListRaw) {
+
+            var tzList = [];
+            var regionList = [];
+            var seen = {};
+            var len = tzListRaw.length;
+            var i, aux;
+
+
+            for (i = 0; i < len; i++) {
+
+                var parsedTz = parseTzId(tzListRaw[i]);
+
+                tzList.push(parsedTz)
+
+                if (!seen.hasOwnProperty(parsedTz.region)) {
+
+                    seen[parsedTz.region] = true;
+                    regionList.push(parsedTz.region);
+                }
+
+            }
+
+
+            return [regionList, tzList];
+        }
+    }
+
+})();
+
+(function() {
+    'use strict';
+
+
+    angular.module('pctDate.utils.tzId.removeTzIdSpecialCases', [])
+        .factory('removeTzIdSpecialCases', factory);
+
+
+    function factory() {
+
+        return function removeTzIdSpecialCases(tzListRaw) {
+            return tzListRaw.filter(function(el) {
+
+                return /^[^\/]+\/.+/.test(el)
+            });
+        };
+
+    };
+
+})();
+
+(function() {
+
+    angular.module('pctDate.utils.tzId', [
+            'pctDate.utils.tzId.parseTzId',
+            'pctDate.utils.tzId.parseTzIdList',
+            'pctDate.utils.tzId.removeTzIdSpecialCases',
+            'pctDate.utils.tzId.filterTzByRegion',
+            'pctDate.utils.tzId.getTzList'
+        ]);
+
+
+}) ();
+
+angular.module("pctDate.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/timezoneSelector/timezoneSelector.tpl.html","<select ng-model=\"ctrl.selectedRegion\" ng-options=\"region for region in ctrl.tzRegionList\" class=\"{{clazz}}\">\n</select>\n\n\n<select ng-model=\"ngModel\" ng-options=\"tz.id as tz.subregion for tz in ctrl.getTzListForRegion(ctrl.selectedRegion) \" class=\"{{clazz}}\">\n</select>\n");}]);
