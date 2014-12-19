638c3c55b5780d63d1cd1c9e426177b2278db6d1
franleplant <franleplant@gmail.com>
2014-12-19 11:46:25 -0300

    add new dist files

--- dist/pctDate.js
+++ dist/pctDate.js
@@ -109,99 +109,149 @@
 })();
 
 (function() {
-    'use strict';
-
-    angular.module('pctDate.timezoneSelector.controller', [
-        'pctDate.utils.tzId',
-        'pctDate.utils.jsTzDetect',
-        'pctDate.utils.tzId.parseTzId'
-        ])
-        .controller('_pctTimezoneSelectorDirectiveController', controller);
-
-
-    controller.$inject = ['$scope', 'getTzList', 'filterTzByRegion', 'jsTzDetect', 'parseTzId'];
-
-    /**
-     * @ngdoc controller
-     * @name _pctTimezoneSelectorDirectiveController
-     * **note** about the name: The underscore and the
-     * weird long and too detaily name are deliberated.
-     * First of all this is a private controller and should
-     * not be used alone.
-     * It main purpose is to server pctTimezoneSelector directive.
-     * And so that's why we don't want to pollute the global
-     * Angular name space, and this our way to try not to.
-     *
-     *
-     * @description
-     * This controller concentrates little to none logic for
-     * the pctTimezoneSelector directive
-     *
-     *
-     *
-     * **Note** about controllerAs and Scope.
-     * We are using as much as we can the controllerAs syntax for
-     * every View-Model attributes (check out the this.attrs in the controllers
-     * body). But, since we need to support Angular 1.2.*, then we need
-     * to bind ngModel the isolated Scope of the directive to achieve
-     * two way data binding.
-     *
-     */
-    function controller($scope, getTzList, filterTzByRegion, jstz, parseTzId) {
-
-
-        //Use js timezone detect javascript library to auto detect the current
-        //user's timezone.
-        var autodetectedTz = parseTzId(jstz.determine().name());
-
-        //Use that auto detected Time Zone as the default
-        //(already selected) option in the time zone selection directive
-        this.selectedRegion = autodetectedTz.region;
-        $scope.ngModel = autodetectedTz.id;
-
-        this.tzRegionList = getTzList().regionList;
-
-        var tzList = getTzList().tzList;
-
-        this.getTzListForRegion = filterTzByRegion(tzList);
-    }
-
-}) ();
-
-(function() {
 
     angular.module('pctDate.timezoneSelector.directive', [
         //Auto generated template module for testing and distribution purposes
         'pctDate.templates',
-        'pctDate.timezoneSelector.controller'
+        'pctDate.utils.tzId',
+        'pctDate.utils.jsTzDetect',
+        'pctDate.utils.tzId.parseTzId'
         ])
         .directive('pctTimezoneSelector', directiveDef);
 
+    directiveDef.$inject = ['getTzList', 'filterTzByRegion', 'jsTzDetect', 'parseTzId'];
 
     /**
      * @ngdoc directive
      * @name pctTimezoneSelector
+     * @restrict E
      * @description
      * Reusable component-like directive to select Time Zones.
      * It is well suited for angular forms.
+     * It is integrated with Time Zone auto detection.
      *
-     * Attribute / parameters
-     * - ngModel: the angular model to use. It will be two way data bind.
-     * - class: The directive is smart enough to apply the same classes applied to the
-     *     custom element to the inner select elements. Feel free to use any bootstrap
-     *     or any other framework or custom css classes.
+     * ## Arguments
+     * @param {String Time Zone Id} ng-model:
+     *      Assign the angular model to use. It will be two way data bound.
+     *      You can use it as and output as an input, just like a regular angular input element
+     * @param {String} class:
+     *      The directive is smart enough to apply the same classes applied to the
+     *      custom element to the inner select elements. Feel free to use any bootstrap
+     *      or any other framework or custom css classes.
+     * @param {Bool} autodetecttz:
+     *      Instruct the directive if you want to attempt
+     *      to auto guess the user's timezone or not. Useful for editing modes.
+     *      The default is false.
+     * @param {Bool} ng-required:
+     *      mark if the time zone selector is a required field or not.
+     * @param {String} name:
+     *      Select input name
+     *
+     *
+     * ## Example
+     * ```html
+     * <pct-timezone-selector
+     *      ng-model="model.tz"
+     *      autodetecttz="true"
+     *      class="c1 c2"
+     *      ng-required="true"
+     *      name="timezone"
+     * >
+     * </pct-timezone-selector>
+     * ```
      *
      *
      */
-    function directiveDef() {
+    function directiveDef(getTzList, filterTzByRegionFactory, jstz, parseTzId) {
 
+        function link(scope, element, attrs, ngModelController) {
+            //TODO: change filterTz... name
+            var filterTzByRegion = filterTzByRegionFactory(getTzList().tzList);
+
+            // autodetecttz attribute logic
+            var autodetectTzFlag =  attrs.autodetecttz === 'true' ? true : false;
+
+            //Initialize where filtered Timezone list will be stored
+            scope.tzList;
+
+            //Initialize where the current selected Region will be stored
+            scope.selectedRegion;
+
+            //Initialize the List of Regions available
+            scope.tzRegionList = getTzList().regionList;
+
+
+            /**
+             * @description
+             * Implement ngModel watch.
+             * This enables the directive to respond correctly to
+             * the ngModel changes originated from the outside of this directive
+             * (by another controller for example) or from inside
+             * of this directive (by the main select element).
+             *
+             * **Note1: **
+             * We need to use a $watch over an ng-change directive
+             * because the latter only responds to user input and we also
+             * need to respond to changes by other components.
+             *
+             * **Note2: **
+             * ngModelController.$render was the first choice over the $watch
+             * but we couldn't make it work.
+             *
+             */
+            scope.$watch(
+                function() {
+                    return ngModelController.$modelValue
+                },
+                function(tzId) {
+                    var tzId = parseTzId(scope.ngModel);
+                    scope.filterTzList(tzId.region);
+                    scope.setSelectedRegion(tzId.id);
+                }
+            );
+
+
+            /**
+             * @name filterTzList
+             * @param {String} Region where TimeZone Id = "Region/SubRegion"
+             *
+             * @description
+             * Main handler for filtering the main select element
+             * list of options by the selected Region
+             *
+             */
+            scope.filterTzList = function(region) {
+                scope.tzList = filterTzByRegion(region)
+            }
+
+            /**
+             * @name setSelectedRegion
+             * @param {String} tzId
+             *
+             * @description
+             * Setter for the selectedRegion.
+             * It extracts the selected Region from the TzId
+             * selected in the main select element (ngModel)
+             *
+             */
+            scope.setSelectedRegion = function(tzId) {
+                scope.selectedRegion = parseTzId(tzId).region;
+            }
+
+            if (autodetectTzFlag) {
+                //Use js timezone detect javascript library to auto detect the current
+                //user's timezone.
+                //Use that auto detected Time Zone as the default
+                //(already selected) option in the time zone selection directive
+                scope.ngModel = parseTzId(jstz.determine().name()).id;
+            }
+        }
 
         return {
             restrict: 'E',
             require: 'ngModel',
-            controller: '_pctTimezoneSelectorDirectiveController',
-            controllerAs: 'ctrl',
             templateUrl: 'src/timezoneSelector/timezoneSelector.tpl.html',
+            link: link,
 
             // Here we are binding in two different ways the directive attributes:
             //
@@ -214,14 +264,12 @@
             // https://umur.io/angularjs-directives-using-isolated-scope-with-attributes/
             scope: {
                 ngModel: '=ngModel',
-                clazz: '@class'
+                clazz: '@class',
+                ngRequired: '@ngRequired',
+                name: '@name'
             }
-
-        }
+        };
     }
-
-
-
 }) ();
 
 (function() {
@@ -605,6 +653,7 @@
 
 })();
 
+
 (function() {
 
     /*
@@ -632,4 +681,4 @@
 
 /*! jstz - v1.0.4 - 2012-12-12 */
 (function(e){var t=function(){"use strict";var e="s",n=function(e){var t=-e.getTimezoneOffset();return t!==null?t:0},r=function(e,t,n){var r=new Date;return e!==undefined&&r.setFullYear(e),r.setDate(n),r.setMonth(t),r},i=function(e){return n(r(e,0,2))},s=function(e){return n(r(e,5,2))},o=function(e){var t=e.getMonth()>7?s(e.getFullYear()):i(e.getFullYear()),r=n(e);return t-r!==0},u=function(){var t=i(),n=s(),r=i()-s();return r<0?t+",1":r>0?n+",1,"+e:t+",0"},a=function(){var e=u();return new t.TimeZone(t.olson.timezones[e])};return{determine:a,date_is_dst:o}}();t.TimeZone=function(e){"use strict";var n=null,r=function(){return n},i=function(){var e=t.olson.ambiguity_list[n],r=e.length,i=0,s=e[0];for(;i<r;i+=1){s=e[i];if(t.date_is_dst(t.olson.dst_start_dates[s])){n=s;return}}},s=function(){return typeof t.olson.ambiguity_list[n]!="undefined"};return n=e,s()&&i(),{name:r}},t.olson={},t.olson.timezones={"-720,0":"Etc/GMT+12","-660,0":"Pacific/Pago_Pago","-600,1":"America/Adak","-600,0":"Pacific/Honolulu","-570,0":"Pacific/Marquesas","-540,0":"Pacific/Gambier","-540,1":"America/Anchorage","-480,1":"America/Los_Angeles","-480,0":"Pacific/Pitcairn","-420,0":"America/Phoenix","-420,1":"America/Denver","-360,0":"America/Guatemala","-360,1":"America/Chicago","-360,1,s":"Pacific/Easter","-300,0":"America/Bogota","-300,1":"America/New_York","-270,0":"America/Caracas","-240,1":"America/Halifax","-240,0":"America/Santo_Domingo","-240,1,s":"America/Santiago","-210,1":"America/St_Johns","-180,1":"America/Godthab","-180,0":"America/Argentina/Buenos_Aires","-180,1,s":"America/Montevideo","-120,0":"Etc/GMT+2","-120,1":"Etc/GMT+2","-60,1":"Atlantic/Azores","-60,0":"Atlantic/Cape_Verde","0,0":"Etc/UTC","0,1":"Europe/London","60,1":"Europe/Berlin","60,0":"Africa/Lagos","60,1,s":"Africa/Windhoek","120,1":"Asia/Beirut","120,0":"Africa/Johannesburg","180,0":"Asia/Baghdad","180,1":"Europe/Moscow","210,1":"Asia/Tehran","240,0":"Asia/Dubai","240,1":"Asia/Baku","270,0":"Asia/Kabul","300,1":"Asia/Yekaterinburg","300,0":"Asia/Karachi","330,0":"Asia/Kolkata","345,0":"Asia/Kathmandu","360,0":"Asia/Dhaka","360,1":"Asia/Omsk","390,0":"Asia/Rangoon","420,1":"Asia/Krasnoyarsk","420,0":"Asia/Jakarta","480,0":"Asia/Shanghai","480,1":"Asia/Irkutsk","525,0":"Australia/Eucla","525,1,s":"Australia/Eucla","540,1":"Asia/Yakutsk","540,0":"Asia/Tokyo","570,0":"Australia/Darwin","570,1,s":"Australia/Adelaide","600,0":"Australia/Brisbane","600,1":"Asia/Vladivostok","600,1,s":"Australia/Sydney","630,1,s":"Australia/Lord_Howe","660,1":"Asia/Kamchatka","660,0":"Pacific/Noumea","690,0":"Pacific/Norfolk","720,1,s":"Pacific/Auckland","720,0":"Pacific/Tarawa","765,1,s":"Pacific/Chatham","780,0":"Pacific/Tongatapu","780,1,s":"Pacific/Apia","840,0":"Pacific/Kiritimati"},t.olson.dst_start_dates=function(){"use strict";var e=new Date(2010,6,15,1,0,0,0);return{"America/Denver":new Date(2011,2,13,3,0,0,0),"America/Mazatlan":new Date(2011,3,3,3,0,0,0),"America/Chicago":new Date(2011,2,13,3,0,0,0),"America/Mexico_City":new Date(2011,3,3,3,0,0,0),"America/Asuncion":new Date(2012,9,7,3,0,0,0),"America/Santiago":new Date(2012,9,3,3,0,0,0),"America/Campo_Grande":new Date(2012,9,21,5,0,0,0),"America/Montevideo":new Date(2011,9,2,3,0,0,0),"America/Sao_Paulo":new Date(2011,9,16,5,0,0,0),"America/Los_Angeles":new Date(2011,2,13,8,0,0,0),"America/Santa_Isabel":new Date(2011,3,5,8,0,0,0),"America/Havana":new Date(2012,2,10,2,0,0,0),"America/New_York":new Date(2012,2,10,7,0,0,0),"Asia/Beirut":new Date(2011,2,27,1,0,0,0),"Europe/Helsinki":new Date(2011,2,27,4,0,0,0),"Europe/Istanbul":new Date(2011,2,28,5,0,0,0),"Asia/Damascus":new Date(2011,3,1,2,0,0,0),"Asia/Jerusalem":new Date(2011,3,1,6,0,0,0),"Asia/Gaza":new Date(2009,2,28,0,30,0,0),"Africa/Cairo":new Date(2009,3,25,0,30,0,0),"Pacific/Auckland":new Date(2011,8,26,7,0,0,0),"Pacific/Fiji":new Date(2010,11,29,23,0,0,0),"America/Halifax":new Date(2011,2,13,6,0,0,0),"America/Goose_Bay":new Date(2011,2,13,2,1,0,0),"America/Miquelon":new Date(2011,2,13,5,0,0,0),"America/Godthab":new Date(2011,2,27,1,0,0,0),"Europe/Moscow":e,"Asia/Yekaterinburg":e,"Asia/Omsk":e,"Asia/Krasnoyarsk":e,"Asia/Irkutsk":e,"Asia/Yakutsk":e,"Asia/Vladivostok":e,"Asia/Kamchatka":e,"Europe/Minsk":e,"Australia/Perth":new Date(2008,10,1,1,0,0,0)}}(),t.olson.ambiguity_list={"America/Denver":["America/Denver","America/Mazatlan"],"America/Chicago":["America/Chicago","America/Mexico_City"],"America/Santiago":["America/Santiago","America/Asuncion","America/Campo_Grande"],"America/Montevideo":["America/Montevideo","America/Sao_Paulo"],"Asia/Beirut":["Asia/Beirut","Europe/Helsinki","Europe/Istanbul","Asia/Damascus","Asia/Jerusalem","Asia/Gaza"],"Pacific/Auckland":["Pacific/Auckland","Pacific/Fiji"],"America/Los_Angeles":["America/Los_Angeles","America/Santa_Isabel"],"America/New_York":["America/Havana","America/New_York"],"America/Halifax":["America/Goose_Bay","America/Halifax"],"America/Godthab":["America/Miquelon","America/Godthab"],"Asia/Dubai":["Europe/Moscow"],"Asia/Dhaka":["Asia/Yekaterinburg"],"Asia/Jakarta":["Asia/Omsk"],"Asia/Shanghai":["Asia/Krasnoyarsk","Australia/Perth"],"Asia/Tokyo":["Asia/Irkutsk"],"Australia/Brisbane":["Asia/Yakutsk"],"Pacific/Noumea":["Asia/Vladivostok"],"Pacific/Tarawa":["Asia/Kamchatka"],"Africa/Johannesburg":["Asia/Gaza","Africa/Cairo"],"Asia/Baghdad":["Europe/Minsk"]},typeof exports!="undefined"?exports.jstz=t:e.jstz=t})(this);
-angular.module("pctDate.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/timezoneSelector/timezoneSelector.tpl.html","<select ng-model=\"ctrl.selectedRegion\" ng-options=\"region for region in ctrl.tzRegionList\" class=\"{{clazz}}\">\n</select>\n\n\n<select ng-model=\"ngModel\" ng-options=\"tz.id as tz.subregion for tz in ctrl.getTzListForRegion(ctrl.selectedRegion) \" class=\"{{clazz}}\">\n</select>\n");}]);
+angular.module("pctDate.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/timezoneSelector/timezoneSelector.tpl.html","<select \n    ng-model=\"selectedRegion\" \n    ng-options=\"region for region in tzRegionList\" \n    ng-change=\"filterTzList(selectedRegion)\"\n    class=\"{{clazz}}\"\n>\n</select>\n\n\n<select \n    ng-model=\"ngModel\" \n    ng-options=\"tz.id as tz.subregion for tz in tzList\" \n    class=\"{{clazz}}\"\n    ng-required=\"{{ngRequired}}\"\n    name={{name}}\n>\n    <option></option>\n</select>\n");}]);
