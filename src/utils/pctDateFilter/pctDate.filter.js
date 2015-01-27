a742e53a9fc1705397ffcee15b324ca91825f460
Fran Guijarro <franleplant@gmail.com>
2015-01-27 19:00:28 -0300

    Add pctDateFilter to format dates with a determined format and timezone

---
+++ src/utils/pctDateFilter/pctDate.filter.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    angular.module('pctDate.utils.pctDateFilter', [
+        'pctMoment'
+    ])
+        .filter('pctDate', pctDateFilterDef);
+
+    pctDateFilterDef.$inject = ['moment'];
+
+    /**
+     * @name pctDate
+     * @description
+     * Useful filter to render dates with a determined format and time zone.
+     * Time Zone parameter should be always present.
+     * The reasons behind the Time Zone parameter being a parameter and not
+     * a state of the filter are the following:
+     * - Making this filter stateless will provide performance improvements when using Angular 1.3+
+     * - Time Zone can change dynamically so this filter abstracts from that and provides maximum flexibility
+     *
+     * @example
+     * {{ aDate | pctDate:format:timeZone}}
+     *
+     * @param {Date} date - Javascript Native Date Object, the date to be converted
+     * @param {string} format - A valid moment.js format string http://momentjs.com/docs/#/displaying/format
+     * @param {string} timeZone - A valid IANA Time Zone Id, for example "Europe/Rome"
+     *
+     * @returns {string} A formatted String that displays a date
+     *
+     */
+    function pctDateFilterDef(moment) {
+        return function pctDateFilter(date, format, timeZone) {
+            if (!timeZone) {
+                throw TypeError('pctDateFilter: timeZone parameter is required');
+            }
+
+            return moment(date).tz(timeZone).format(format);
+        }
+    }
+})();
---
+++ src/utils/pctDateFilter/pctDate.filter.spec.js
@@ -0,0 +1,36 @@
+ describe('pctDate.utils.pctDateFilter.pctDate module', function() {
+    'use strict';
+
+    var pctDate, moment;
+    var d = new Date();
+    var timeZone1 = 'Europe/Rome';
+    var timeZone2 = 'America/Los_Angeles';
+    var format1 = 'dddd, MMMM Do YYYY, h:mm:ss a';
+    var format2 = '';
+    // Include the main module
+    beforeEach(module('pctDate.utils.pctDateFilter'));
+
+    // Get a reference of the service to test
+    beforeEach(inject(function($injector) {
+        moment = $injector.get('moment');
+        pctDate = $injector.get('pctDateFilter');
+    }));
+
+
+    it('should return a valid date formatted string', function() {
+        expect(pctDate(d, format1, timeZone1)).toBe(moment.tz(timeZone1).format(format1));
+        expect(pctDate(d, format1, timeZone2)).toBe(moment.tz(timeZone2).format(format1));
+    });
+
+    it('should return a valid date formatted string when the format is empty', function() {
+        expect(pctDate(d, format2, timeZone1)).toBe(moment.tz(timeZone1).format(format2));
+    });
+
+    describe('invalid parameteres', function() {
+        it('should throw an error when "timeZone" is not specified', function() {
+            expect(function() { pctDate(d, format1) }).toThrow();
+        });
+
+    });
+
+});
