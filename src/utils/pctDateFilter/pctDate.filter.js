8c11499ef82b0af903a0fa69955fa2d7d68c41eb
Fran Guijarro <franleplant@gmail.com>
2015-01-28 11:01:25 -0300

    PCT-1192: Add 'from' functionality to the pctDateFilter

--- src/utils/pctDateFilter/pctDate.filter.js
+++ src/utils/pctDateFilter/pctDate.filter.js
@@ -12,26 +12,49 @@
      * @name pctDate
      * @description
      * Useful filter to render dates with a determined format and time zone.
+     * If the format parameter is a Date then it will return a "From" string,
+     * i.e "A few seconds ago"
+     *
      * Time Zone parameter should be always present.
-     * The reasons behind the Time Zone parameter being a parameter and not
+     * The reasons behind the Time Zone being a parameter and not
      * a state of the filter are the following:
-     * - Making this filter stateless will provide performance improvements when using Angular 1.3+
-     * - Time Zone can change dynamically so this filter abstracts from that and provides maximum flexibility
+     * - Making this filter stateless will provide performance
+     *      improvements when using Angular 1.3+
+     * - Time Zone can change dynamically so this filter abstracts
+     *      from that and provides maximum flexibility
      *
      * @example
-     * {{ aDate | pctDate:format:timeZone}}
+     * {{ date | pctDate:timeZone:format }} //=> A formated Date String
+     * {{ date | pctDate:timeZone:fromNow }} //=> Something like "A year ago"
      *
-     * @param {Date} date - Javascript Native Date Object, the date to be converted
+     *
+     * @param {Date} date - Javascript Native Date Object, the input date.
      * @param {string} timeZone - A valid IANA Time Zone Id, for example "Europe/Rome"
-     * @param {string} format - A valid moment.js format string http://momentjs.com/docs/#/displaying/format
+     * @param {string|Date} format - A valid moment.js format string
+     *      http://momentjs.com/docs/#/displaying/format or a Javascript Native Date Object.
+     *       - If it is a Moment's format string then this filter will just format the date input
+     *          with the correct time zone.
+     *       - If the parameter is a Date Object then this filter will switch it's functionality
+     *          to the Moment's `from()` API URL
+     *          Use a `new Date()` object to achieve the same as Moment's  `fromNow()` API
      *
      * @returns {string} A formatted String that displays a date
      *
      */
     function pctDateFilterDef(moment) {
         return function pctDateFilter(date, timeZone, format) {
+            var fromDate;
+
             if (!timeZone) {
                 throw TypeError('pctDateFilter: timeZone parameter is required');
+            }
+
+            // if format is date then this filter is being used
+            // to express a moment's "from" String
+            // i.e: "a year ago"
+            if (toString.call(format) === '[object Date]') {
+                fromDate = format;
+                return moment(date).tz(timeZone).from(fromDate);
             }
 
             return moment(date).tz(timeZone).format(format);
--- src/utils/pctDateFilter/pctDate.filter.spec.js
+++ src/utils/pctDateFilter/pctDate.filter.spec.js
@@ -18,19 +18,28 @@
 
 
     it('should return a valid date formatted string', function() {
-        expect(pctDate(d, timeZone1, format1)).toBe(moment.tz(timeZone1).format(format1));
-        expect(pctDate(d, timeZone2, format1)).toBe(moment.tz(timeZone2).format(format1));
+        expect(pctDate(d, timeZone1, format1)).toBe(moment(d).tz(timeZone1).format(format1));
+        expect(pctDate(d, timeZone2, format1)).toBe(moment(d).tz(timeZone2).format(format1));
     });
 
     it('should return a valid date formatted string when the format is empty', function() {
-        expect(pctDate(d, timeZone1, format2)).toBe(moment.tz(timeZone1).format(format2));
+        expect(pctDate(d, timeZone1, format2)).toBe(moment(d).tz(timeZone1).format(format2));
+    });
+
+    it('should return a "From" String when the format parameter is a Date', function() {
+        // Random date that points to September 2014
+        var fromDate = new Date(1412052562440);
+        expect(pctDate(d, timeZone1, fromDate)).toBe(moment(d).tz(timeZone1).from(fromDate));
+    });
+
+    it('should return a "FromNow" String when the format parameter is the current Date', function() {
+        expect(pctDate(d, timeZone1, d)).toBe(moment(d).tz(timeZone1).fromNow());
     });
 
     describe('invalid parameteres', function() {
         it('should throw an error when "timeZone" is not specified', function() {
             expect(function() { pctDate(d) }).toThrow();
         });
-
     });
 
 });
