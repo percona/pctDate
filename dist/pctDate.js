aede17f508d750454d5e51915593eec0f9375769
Fran Guijarro <franleplant@gmail.com>
2015-02-02 16:31:59 -0300

    add new dist files

--- dist/pctDate.js
+++ dist/pctDate.js
@@ -1,4 +1,60 @@
 (function() {
+    'use strict';
+
+
+    angular.module('pctDate.config', [
+        'pctMoment'
+    ])
+        .factory('pctDateConfig', pctDateConfigFactory);
+
+
+    pctDateConfigFactory.$inject = ['moment'];
+
+    /**
+     * @description
+     * Global configuration for pctDate utils
+     *
+     * For now the only available configuration is timeZone.
+     * This config object has been created with special getters and setters
+     * to enable shared global state.
+     * Also, on set, the config object will validate that the newTimezone is
+     * a valid one, if not, throwing an exception.
+     *
+     * @example
+     * //Set
+     * pctDateConfig.timeZone = 'Europe/Rome'
+     * //Get
+     * pctDateConfig.timeZone // => 'Europe/Rome'
+     *
+     *
+     * @returns {Object}
+     *
+     *
+     */
+    function pctDateConfigFactory(moment) {
+
+        var _timeZone = 'Etc/UTC';
+
+
+        var config = {
+            get timeZone() {
+                return _timeZone;
+            },
+
+            set timeZone(newTz) {
+
+                if (!moment.tz.zone(newTz)) {
+                    throw 'pctDateConfig: ' + newTz + ' is not a valid Time Zone ID'
+                }
+                _timeZone = newTz;
+            }
+        };
+
+        return config;
+    }
+})();
+
+(function() {
 
     angular.module('pctDate.isDate', [])
         .factory('isDate', isDateService);
@@ -49,11 +105,13 @@
      *
      */
     angular.module('pctDate', [
+        'pctDate.config',
         'pctDate.toUnixTs',
         'pctDate.isDate',
         'pctDate.timezoneSelector',
         'pctDate.utils.jsTzDetect',
-        'pctDate.utils.pctDateFilter'
+        'pctDate.utils.pctDateFilter',
+        'pctDate.utils.pctDateFromFilter'
     ]);
 
 }) ();
@@ -327,64 +385,134 @@
 (function() {
     'use strict';
 
+    angular.module('pctDate.utils.jsTzDetect', [])
+        .factory('jsTzDetect', factory);
+
+
+    factory.$inject = ['$window'];
+
+    /**
+     * @ngdoc service
+     * @name jsTzDetect
+     * @description
+     *
+     * Wrapper for jstimezonedetect library.
+     * It's API is pretty slim and you can check it out here:
+     * https://bitbucket.org/pellepim/jstimezonedetect
+     *
+     * @returns {Object} jstz reference
+     *
+     *
+     */
+    function factory($window) {
+        //Since jstz will be concatenated with pctDate we dont need to check
+        //if it has been loaded correctly
+        return $window.jstz;
+    }
+})();
+
+(function() {
+    'use strict';
+
     angular.module('pctDate.utils.pctDateFilter', [
+        'pctDate.config',
         'pctMoment'
     ])
         .filter('pctDate', pctDateFilterDef);
 
-    pctDateFilterDef.$inject = ['moment'];
+    pctDateFilterDef.$inject = ['moment', 'pctDateConfig'];
 
     /**
      * @name pctDate
      * @description
-     * Useful filter to render dates with a determined format and time zone.
-     * If the format parameter is a Date then it will return a "From" string,
-     * i.e "A few seconds ago"
+     * Useful filter to render dates with a determined format, using `format` Moment method.
+     * This filter uses the pctDateConfig.timeZone value to compute properly time zone setted dates.
      *
-     * Time Zone parameter should be always present.
-     * The reasons behind the Time Zone being a parameter and not
-     * a state of the filter are the following:
-     * - Making this filter stateless will provide performance
-     *      improvements when using Angular 1.3+
-     * - Time Zone can change dynamically so this filter abstracts
-     *      from that and provides maximum flexibility
+     * - `format` Moment API: http://momentjs.com/docs/#/displaying/format
+     *
+     * **Note**
+     * This filter is stateful because it depends on the pctDateConfig.timeZone value
+     * (only applicable to Angular 1.3+), see API https://docs.angularjs.org/guide/filter
      *
      * @example
-     * {{ date | pctDate:timeZone:format }} //=> A formated Date String
-     * {{ date | pctDate:timeZone:fromNow }} //=> Something like "A year ago"
+     * {{ date | pctDate:format }} //=> A formated Date String in the pctDateConfig.timeZone
      *
      *
      * @param {Date} date - Javascript Native Date Object, the input date.
-     * @param {string} timeZone - A valid IANA Time Zone Id, for example "Europe/Rome"
-     * @param {string|Date} format - A valid moment.js format string
-     *      http://momentjs.com/docs/#/displaying/format or a Javascript Native Date Object.
-     *       - If it is a Moment's format string then this filter will just format the date input
-     *          with the correct time zone.
-     *       - If the parameter is a Date Object then this filter will switch it's functionality
-     *          to the Moment's `from()` API http://momentjs.com/docs/#/displaying/from/
-     *          Use a `new Date()` object to achieve the same as Moment's  `fromNow()` API
+     * @param {string} format - A valid moment.js format string
      *
      * @returns {string} A formatted String that displays a date
      *
      */
-    function pctDateFilterDef(moment) {
-        return function pctDateFilter(date, timeZone, format) {
+    function pctDateFilterDef(moment, pctDateConfig) {
+        //Mark this filter as stateful (Angular 1.3+)
+        pctDateFilter.$stateful = true;
+        function pctDateFilter(date, format) {
             var fromDate;
 
-            if (!timeZone) {
-                throw TypeError('pctDateFilter: timeZone parameter is required');
-            }
-
-            // if format is date then this filter is being used
-            // to express a moment's "from" String
-            // i.e: "a year ago"
-            if (toString.call(format) === '[object Date]') {
-                fromDate = format;
-                return moment(date).tz(timeZone).from(fromDate);
-            }
-
-            return moment(date).tz(timeZone).format(format);
+            return moment(date).tz(pctDateConfig.timeZone).format(format);
         }
+        return pctDateFilter;
+    }
+})();
+
+(function() {
+    'use strict';
+
+    angular.module('pctDate.utils.pctDateFromFilter', [
+        'pctDate.config',
+        'pctMoment'
+    ])
+        .filter('pctDateFrom', pctDateFromFilterDef);
+
+    pctDateFromFilterDef.$inject = ['moment', 'pctDateConfig'];
+
+    /**
+     * @name pctDateFrom
+     * @description
+     * Useful filter to render From strings using `from` Moment method.
+     * If the fromDate parameter is null then this filter will render `fromNow` Moment method.
+     * This filter uses the pctDateConfig.timeZone value to properly shift formatted dates with a time zone.
+     *
+     * - `from` Moment API: http://momentjs.com/docs/#/displaying/from
+     * - `fromNow` Moment API: http://momentjs.com/docs/#/displaying/fromNow
+     *
+     * **Note**
+     * This filter is stateful because it depends on the pctDateConfig.timeZone value
+     * (only applicable to Angular 1.3+), see API https://docs.angularjs.org/guide/filter
+     *
+     *
+     * @example
+     * {{ date | pctDate:fromDate }} //=> Something like "A year ago"
+     *
+     *
+     * @param {Date} date - Javascript Native Date Object, the input date.
+     * @param {Date|null} fromDate - A Javascript Native Date Object.
+     *
+     * @returns {string} A formatted From String
+     *
+     */
+    function pctDateFromFilterDef(moment, pctDateConfig) {
+
+        //Mark this filter as stateful (only for Angular 1.3)
+        pctDateFromFilter.$stateful = true;
+
+        function pctDateFromFilter(date, fromDate) {
+
+            // If fromDate is not null and it is not a Date object then throw!
+            if (fromDate &&  toString.call(fromDate) !== '[object Date]') {
+                throw 'pctDateFromFilter: format should be a Native Date object';
+            }
+
+            //If fromDate is null the return fromNow() (see Moment.js API)
+            if (!fromDate) {
+                return moment(date).tz(pctDateConfig.timeZone).fromNow();
+            }
+
+            return moment(date).tz(pctDateConfig.timeZone).from(fromDate);
+        }
+
+        return pctDateFromFilter;
     }
 })();
 
@@ -756,35 +884,6 @@
 
 
 }) ();
-
-(function() {
-    'use strict';
-
-    angular.module('pctDate.utils.jsTzDetect', [])
-        .factory('jsTzDetect', factory);
-
-
-    factory.$inject = ['$window'];
-
-    /**
-     * @ngdoc service
-     * @name jsTzDetect
-     * @description
-     *
-     * Wrapper for jstimezonedetect library.
-     * It's API is pretty slim and you can check it out here:
-     * https://bitbucket.org/pellepim/jstimezonedetect
-     *
-     * @returns {Object} jstz reference
-     *
-     *
-     */
-    function factory($window) {
-        //Since jstz will be concatenated with pctDate we dont need to check
-        //if it has been loaded correctly
-        return $window.jstz;
-    }
-})();
 
 /*! jstz - v1.0.4 - 2012-12-12 */
 (function(e){var t=function(){"use strict";var e="s",n=function(e){var t=-e.getTimezoneOffset();return t!==null?t:0},r=function(e,t,n){var r=new Date;return e!==undefined&&r.setFullYear(e),r.setDate(n),r.setMonth(t),r},i=function(e){return n(r(e,0,2))},s=function(e){return n(r(e,5,2))},o=function(e){var t=e.getMonth()>7?s(e.getFullYear()):i(e.getFullYear()),r=n(e);return t-r!==0},u=function(){var t=i(),n=s(),r=i()-s();return r<0?t+",1":r>0?n+",1,"+e:t+",0"},a=function(){var e=u();return new t.TimeZone(t.olson.timezones[e])};return{determine:a,date_is_dst:o}}();t.TimeZone=function(e){"use strict";var n=null,r=function(){return n},i=function(){var e=t.olson.ambiguity_list[n],r=e.length,i=0,s=e[0];for(;i<r;i+=1){s=e[i];if(t.date_is_dst(t.olson.dst_start_dates[s])){n=s;return}}},s=function(){return typeof t.olson.ambiguity_list[n]!="undefined"};return n=e,s()&&i(),{name:r}},t.olson={},t.olson.timezones={"-720,0":"Etc/GMT+12","-660,0":"Pacific/Pago_Pago","-600,1":"America/Adak","-600,0":"Pacific/Honolulu","-570,0":"Pacific/Marquesas","-540,0":"Pacific/Gambier","-540,1":"America/Anchorage","-480,1":"America/Los_Angeles","-480,0":"Pacific/Pitcairn","-420,0":"America/Phoenix","-420,1":"America/Denver","-360,0":"America/Guatemala","-360,1":"America/Chicago","-360,1,s":"Pacific/Easter","-300,0":"America/Bogota","-300,1":"America/New_York","-270,0":"America/Caracas","-240,1":"America/Halifax","-240,0":"America/Santo_Domingo","-240,1,s":"America/Santiago","-210,1":"America/St_Johns","-180,1":"America/Godthab","-180,0":"America/Argentina/Buenos_Aires","-180,1,s":"America/Montevideo","-120,0":"Etc/GMT+2","-120,1":"Etc/GMT+2","-60,1":"Atlantic/Azores","-60,0":"Atlantic/Cape_Verde","0,0":"Etc/UTC","0,1":"Europe/London","60,1":"Europe/Berlin","60,0":"Africa/Lagos","60,1,s":"Africa/Windhoek","120,1":"Asia/Beirut","120,0":"Africa/Johannesburg","180,0":"Asia/Baghdad","180,1":"Europe/Moscow","210,1":"Asia/Tehran","240,0":"Asia/Dubai","240,1":"Asia/Baku","270,0":"Asia/Kabul","300,1":"Asia/Yekaterinburg","300,0":"Asia/Karachi","330,0":"Asia/Kolkata","345,0":"Asia/Kathmandu","360,0":"Asia/Dhaka","360,1":"Asia/Omsk","390,0":"Asia/Rangoon","420,1":"Asia/Krasnoyarsk","420,0":"Asia/Jakarta","480,0":"Asia/Shanghai","480,1":"Asia/Irkutsk","525,0":"Australia/Eucla","525,1,s":"Australia/Eucla","540,1":"Asia/Yakutsk","540,0":"Asia/Tokyo","570,0":"Australia/Darwin","570,1,s":"Australia/Adelaide","600,0":"Australia/Brisbane","600,1":"Asia/Vladivostok","600,1,s":"Australia/Sydney","630,1,s":"Australia/Lord_Howe","660,1":"Asia/Kamchatka","660,0":"Pacific/Noumea","690,0":"Pacific/Norfolk","720,1,s":"Pacific/Auckland","720,0":"Pacific/Tarawa","765,1,s":"Pacific/Chatham","780,0":"Pacific/Tongatapu","780,1,s":"Pacific/Apia","840,0":"Pacific/Kiritimati"},t.olson.dst_start_dates=function(){"use strict";var e=new Date(2010,6,15,1,0,0,0);return{"America/Denver":new Date(2011,2,13,3,0,0,0),"America/Mazatlan":new Date(2011,3,3,3,0,0,0),"America/Chicago":new Date(2011,2,13,3,0,0,0),"America/Mexico_City":new Date(2011,3,3,3,0,0,0),"America/Asuncion":new Date(2012,9,7,3,0,0,0),"America/Santiago":new Date(2012,9,3,3,0,0,0),"America/Campo_Grande":new Date(2012,9,21,5,0,0,0),"America/Montevideo":new Date(2011,9,2,3,0,0,0),"America/Sao_Paulo":new Date(2011,9,16,5,0,0,0),"America/Los_Angeles":new Date(2011,2,13,8,0,0,0),"America/Santa_Isabel":new Date(2011,3,5,8,0,0,0),"America/Havana":new Date(2012,2,10,2,0,0,0),"America/New_York":new Date(2012,2,10,7,0,0,0),"Asia/Beirut":new Date(2011,2,27,1,0,0,0),"Europe/Helsinki":new Date(2011,2,27,4,0,0,0),"Europe/Istanbul":new Date(2011,2,28,5,0,0,0),"Asia/Damascus":new Date(2011,3,1,2,0,0,0),"Asia/Jerusalem":new Date(2011,3,1,6,0,0,0),"Asia/Gaza":new Date(2009,2,28,0,30,0,0),"Africa/Cairo":new Date(2009,3,25,0,30,0,0),"Pacific/Auckland":new Date(2011,8,26,7,0,0,0),"Pacific/Fiji":new Date(2010,11,29,23,0,0,0),"America/Halifax":new Date(2011,2,13,6,0,0,0),"America/Goose_Bay":new Date(2011,2,13,2,1,0,0),"America/Miquelon":new Date(2011,2,13,5,0,0,0),"America/Godthab":new Date(2011,2,27,1,0,0,0),"Europe/Moscow":e,"Asia/Yekaterinburg":e,"Asia/Omsk":e,"Asia/Krasnoyarsk":e,"Asia/Irkutsk":e,"Asia/Yakutsk":e,"Asia/Vladivostok":e,"Asia/Kamchatka":e,"Europe/Minsk":e,"Australia/Perth":new Date(2008,10,1,1,0,0,0)}}(),t.olson.ambiguity_list={"America/Denver":["America/Denver","America/Mazatlan"],"America/Chicago":["America/Chicago","America/Mexico_City"],"America/Santiago":["America/Santiago","America/Asuncion","America/Campo_Grande"],"America/Montevideo":["America/Montevideo","America/Sao_Paulo"],"Asia/Beirut":["Asia/Beirut","Europe/Helsinki","Europe/Istanbul","Asia/Damascus","Asia/Jerusalem","Asia/Gaza"],"Pacific/Auckland":["Pacific/Auckland","Pacific/Fiji"],"America/Los_Angeles":["America/Los_Angeles","America/Santa_Isabel"],"America/New_York":["America/Havana","America/New_York"],"America/Halifax":["America/Goose_Bay","America/Halifax"],"America/Godthab":["America/Miquelon","America/Godthab"],"Asia/Dubai":["Europe/Moscow"],"Asia/Dhaka":["Asia/Yekaterinburg"],"Asia/Jakarta":["Asia/Omsk"],"Asia/Shanghai":["Asia/Krasnoyarsk","Australia/Perth"],"Asia/Tokyo":["Asia/Irkutsk"],"Australia/Brisbane":["Asia/Yakutsk"],"Pacific/Noumea":["Asia/Vladivostok"],"Pacific/Tarawa":["Asia/Kamchatka"],"Africa/Johannesburg":["Asia/Gaza","Africa/Cairo"],"Asia/Baghdad":["Europe/Minsk"]},typeof exports!="undefined"?exports.jstz=t:e.jstz=t})(this);
