b70dcb080345a5656f48ae814bdee6d1c038b0b0
Fran Guijarro <franleplant@gmail.com>
2015-02-20 15:49:23 -0300

    PCT-1194: improve test coverage

--- src/utils/fancyDateRangeFilter/fancyDateRange.filter.js
+++ src/utils/fancyDateRangeFilter/fancyDateRange.filter.js
@@ -11,20 +11,25 @@
     /**
      * Returns a Fancy Formatted Date/Time Range.
      *
-     * The Fancy Date Range timerange has the following formats:
-     * - Same day:   April 16 2014, 20:42 to 21:42
-     * - Same month: Oct 2014, Sat 4 16:08 - Mon 6 15:07
-     * - Same year:  March 16, 21:43 to April 16, 21:43 (2014)
-     * - nothing is the same: December 3 2012, 13:44 to April 16 2014, 21:44
+     * The Fancy Date Range timerange has the following formats according to
+     * the relationship between start and end parameters:
+     * - Different time:
+     *      - April 16 2014, 20:42 to 21:42
+     * - Different time and day:
+     *      - Oct 2014, Sat 4 16:08 - Mon 6 15:07
+     * - Same year only:
+     *      - March 16, 21:43 to April 16, 21:43 (2014)
+     * - Different everything:
+     *      - December 3 2012, 13:44 to April 16 2014, 21:44
      *
      * Depending if start and end are in the same year, same month
      * and / or same day the return value: timerange will change according to the
      * the list above.
      *
-     * @param {Date} start - Lower limit Date object
-     * @param {Date} end   - Upper limit Date object
+     * @param {Date} start - Lower limit Javascript Native Date object
+     * @param {Date} end   - Upper limit Javascript Native Date object
      *
-     * @return {String} timerange - Fancy Date Range
+     * @return {String}
      */
     function fancyDateRangeFilter(moment) {
         return function (start, end) {
@@ -37,14 +42,14 @@
             var mStart = moment(start);
             var mEnd = moment(end);
 
-            // different time
+            // Same year, month and day
             // ie: April 16 2014, 20:42 to 21:42
             if ( sameYear && sameMonth && sameDay) {
                 return mStart.format('MMM D YYYY, HH:mm') +
                         ' to ' + mEnd.format('HH:mm');
             }
 
-            // different day and time
+            // Same year and month and different day
             // ie: Oct 2014, Sat 4 16:08 - Mon 6 15:07 (PCT-981)
             if ( sameYear && sameMonth && !sameDay) {
                 return mStart.format('MMM YYYY') +
@@ -52,15 +57,15 @@
                         ' - ' + mEnd.format('ddd D HH:mm');
             }
 
-            // only same year
+            // Same year, different month (day does not matter)
             // ie: March 16, 21:43 to April 16, 21:43 (2014)
-            if (sameYear && !sameMonth && !sameDay) {
+            if (sameYear && !sameMonth) {
                 return mStart.format('MMM D, HH:mm') +
                         ' to ' + mEnd.format('MMM D, HH:mm (YYYY)');
             }
 
             // default
-            // Everything different
+            // Different year, (month and day does not matter)
             // ie: December 3 2012, 13:44 to April 16 2014, 21:44
             return mStart.format('MMM D YYYY, HH:mm') +
                         ' to ' + mEnd.format('MMM D YYYY, HH:mm');
--- src/utils/fancyDateRangeFilter/fancyDateRange.filter.spec.js
+++ src/utils/fancyDateRangeFilter/fancyDateRange.filter.spec.js
@@ -10,28 +10,57 @@ describe('pctDate.utils.fancyDateRangeFilter module', function () {
     }));
 
 
-    it('should show the correct fancy range for several days from different months', function () {
-        var start = new Date(2014, 7, 27, 18, 26, 13);
-        var end   = new Date(2014, 8, 2, 11, 20 , 7);
-        var fancy = filter(start, end);
+    describe('when year, month and day are the same', function() {
+        it('should show the correct fancy range', function () {
+            var start = new Date(2014, 7, 31, 0, 24, 31);
+            var end   = new Date(2014, 7, 31, 8, 54, 41);
+            var fancy = filter(start, end);
 
-
-        expect(fancy).toBe('Aug 27, 18:26 to Sep 2, 11:20 (2014)');
+            expect(fancy).toBe('Aug 31 2014, 00:24 to 08:54');
+        });
     });
 
-    it('should show the correct fancy range for the same day', function () {
-        var start = new Date(2014, 7, 31, 0, 24, 31);
-        var end   = new Date(2014, 7, 31, 8, 54, 41);
-        var fancy = filter(start, end);
+    describe('when year and month are the same and the day is different', function() {
+        it('should show the correct fancy range', function () {
+            var start = new Date(2014, 1, 13, 16, 24, 10);
+            var end   = new Date(2014, 1, 24, 16, 10, 21);
+            var fancy = filter(start, end);
 
-        expect(fancy).toBe('Aug 31 2014, 00:24 to 08:54');
+            expect(fancy).toBe('Feb 2014, Thu 13 16:24 - Mon 24 16:10');
+        });
     });
 
-    it('should show the correct fancy range for several days from the same month', function () {
-        var start = new Date(2014, 1, 13, 16, 24, 10);
-        var end   = new Date(2014, 1, 24, 16, 10, 21);
-        var fancy = filter(start, end);
+    describe('when year is the same and month is different', function() {
+        it('should show the correct fancy range when days are different', function () {
+            var start = new Date(2014, 7, 27, 18, 26, 13);
+            var end   = new Date(2014, 8, 2, 11, 20 , 7);
+            var fancy = filter(start, end);
 
-        expect(fancy).toBe('Feb 2014, Thu 13 16:24 - Mon 24 16:10');
+            expect(fancy).toBe('Aug 27, 18:26 to Sep 2, 11:20 (2014)');
+        });
+
+        it('should show the correct fancy range when days are the same', function () {
+            var start = new Date(2014, 7, 2, 18, 26, 13);
+            var end   = new Date(2014, 8, 2, 11, 20 , 7);
+            var fancy = filter(start, end);
+
+            expect(fancy).toBe('Aug 2, 18:26 to Sep 2, 11:20 (2014)');
+        });
     });
+
+    describe('when year is different', function() {
+        it('should show the correct fancy range when month are different', function () {
+            var start = new Date(2014, 7, 2, 18, 26, 13);
+            var end   = new Date(2015, 8, 2, 11, 20 , 7);
+
+            expect(filter(start, end)).toBe('Aug 2 2014, 18:26 to Sep 2 2015, 11:20');
+        });
+
+        it('should show the correct fancy range when month are the same', function () {
+            var start = new Date(2014, 8, 2, 18, 26, 13);
+            var end   = new Date(2015, 8, 2, 11, 20 , 7);
+
+            expect(filter(start, end)).toBe('Sep 2 2014, 18:26 to Sep 2 2015, 11:20');
+        });
+    })
 });
